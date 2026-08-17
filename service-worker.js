5f0ea5e7ca2f41e616d09120dc8b942c79a64faf
ROSECC0719 <abby780719@gmail.com>
2026-08-17 23:21:15 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='539-ultimate-v50-20260815';
+const CACHE='539-ultimate-v51-20260817-fix1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./service-worker.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
