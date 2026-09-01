d262a80e6838003ab2cb4bc1fba9956a064a04bf
ROSECC0719 <abby780719@gmail.com>
2026-09-02 00:25:40 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='v6.18-ai-double100-hotfix1';
+const CACHE_VERSION='v6.18.2-nextmap-headtail';
 self.addEventListener('install',event=>{self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE_VERSION).map(k=>caches.delete(k)));await self.clients.claim()})())});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)))});
