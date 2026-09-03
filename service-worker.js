de5a5fc6b6f626ca84fe5fdc709273169dd04101
ROSECC0719 <abby780719@gmail.com>
2026-09-04 02:30:55 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='v6.19.2-head-break-rules';
+const CACHE_VERSION='v6.19.3-rule-search';
 self.addEventListener('install',event=>{self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE_VERSION).map(k=>caches.delete(k)));await self.clients.claim()})())});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)))});
