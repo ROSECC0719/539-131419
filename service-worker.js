9cd0e5cae05c6321cc26cd29a767d15e224113a6
ROSECC0719 <abby780719@gmail.com>
2026-09-04 22:31:10 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='539-pro-v6.20.0-star-intersection-pick-gap-safe';
+const CACHE='539-pro-v6.20.1-star-union-full-history-final-pick';
 const ASSETS=['./','./index.html','./README.md'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
